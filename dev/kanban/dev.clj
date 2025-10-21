dcdcfa904af5cca03aed9455b336200d6e18a66c
Christian Johansen <christian@cjohansen.no>
2025-10-21 19:26:50 +0200

    Add clj-reload and an s-: menu

--- dev/kanban/dev.clj
+++ dev/kanban/dev.clj
@@ -1,5 +1,26 @@
-(ns kanban.dev)
+(ns kanban.dev
+  (:require [clj-reload.core :as clj-reload]
+            [kanban.server :as server]))
 
-(comment
+(def server (atom nil))
+
+(defn start []
+  (when-not @server
+    (let [port 8088]
+      (reset! server (server/start-server port))
+      (println (str "Started server on http://localhost:" port)))))
+
+(defn stop []
+  (some-> @server server/stop-server))
+
+(defn reset []
+  (stop)
+  (clj-reload/reload)
+  ((requiring-resolve `start)))
+
+(comment ;; s-:
+  (start)
+  (stop)
+  (reset)
   (set! *print-namespace-maps* false)
   )
